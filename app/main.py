e1d58e930601148fe722bb6fafabf5bc2d897955
Aditia Prabowo <aditiaprabowo3@gmail.com>
2025-06-09 09:42:34 +0700

    add cors error

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from app.model import load_model, predict
 from app.schemas import PredictResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 model = load_model()
 
